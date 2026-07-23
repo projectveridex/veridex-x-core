edc8102262b2909738f280acc172c4c545d1c691
projectveridex <projectveridex@gmail.com>
2026-07-23 22:20:20 +0100

    Add GitHub opportunity scanning functionality
    Implement GitHub Opportunity Hunter to scan for open source contributions.

---
+++ core/core/core/hunters/github_hunter.py
@@ -0,0 +1,22 @@
+"""
+VERIDEX X
+GitHub Opportunity Hunter
+"""
+
+def scan_github():
+    opportunities = []
+
+    sample_opportunity = {
+        "source": "GitHub",
+        "title": "Open source contribution opportunity",
+        "description": "Developer needed for project improvement",
+        "crypto_payment": False,
+        "low_competition": True,
+        "high_frequency": True,
+        "good_payment": False,
+        "easy_to_complete": True
+    }
+
+    opportunities.append(sample_opportunity)
+
+    return opportunities
