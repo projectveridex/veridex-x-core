3ba54a075abf23f93c7ae8245d168016125ee3e2
projectveridex <projectveridex@gmail.com>
2026-07-24 10:43:03 +0100

    Add TaskPlanner class with planning steps

---
+++ core/core/core/core/task_planner.py
@@ -0,0 +1,27 @@
+"""
+==========================================
+VERIDEX X
+TASK PLANNER
+==========================================
+"""
+
+
+class TaskPlanner:
+
+    def plan(self, opportunity):
+
+        return [
+
+            "Read client request",
+
+            "Break work into steps",
+
+            "Generate solution",
+
+            "Self review",
+
+            "Prepare delivery",
+
+            "Wait for boss approval"
+
+        ]
