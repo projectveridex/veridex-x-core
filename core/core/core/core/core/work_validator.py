9f82838633e8ce47baae77e52b5d5314c35094a0
projectveridex <projectveridex@gmail.com>
2026-07-24 10:43:48 +0100

    Add WorkValidator class with validation method

---
+++ core/core/core/core/core/work_validator.py
@@ -0,0 +1,17 @@
+"""
+==========================================
+VERIDEX X
+WORK VALIDATOR
+==========================================
+"""
+
+
+class WorkValidator:
+
+    def validate(self, work):
+
+        if work is None:
+
+            return False
+
+        return True
