ddc30bfa6ec4b205090068833ebd749257fbb734
projectveridex <projectveridex@gmail.com>
2026-07-24 10:13:10 +0100

    Add GitLab opportunity hunter script

---
+++ hunters/gitlab_hunter.py
@@ -0,0 +1,23 @@
+"""
+VERIDEX X
+GitLab Opportunity Hunter
+"""
+
+def scan_gitlab():
+
+    opportunities = []
+
+    sample = {
+        "source": "GitLab",
+        "title": "Open source issue",
+        "description": "Public repository looking for contributors",
+        "crypto_payment": False,
+        "low_competition": True,
+        "high_frequency": True,
+        "good_payment": False,
+        "easy_to_complete": True
+    }
+
+    opportunities.append(sample)
+
+    return opportunities
